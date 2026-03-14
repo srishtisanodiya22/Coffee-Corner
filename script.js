aad4e51eb95b872c1f93ebc4c4f3111a3e7b5707
Srishti Sanodiya <srishtisanodiya2004@gmail.com>
2026-03-15 00:59:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click',function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).
+        scrollIntoView({
+            behavior: 'smooth'
+        }); 
+    });
+});
+
+const productCards = document.querySelectorAll('.product-card');
+productCards.forEach(card => {
+    card.addEventListener('mouseenter',function() {
+        this.style.transform = 'translateY(0)';
+    });
+});
